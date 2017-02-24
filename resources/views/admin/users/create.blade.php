6aaf138904fbe44d7ef8581af500a39ad45da7bc
mikavoid <mikavoid@gmail.com>
2017-02-24 12:41:18 +0100

    users creation without photos

---
+++ resources/views/admin/users/create.blade.php
@@ -0,0 +1,46 @@
+@extends('layouts.admin')
+
+@section('content')
+    <h1>Create Users</h1>
+
+    {!! Form::open(['action' => 'AdminUsersController@store', 'files' => true]) !!}
+
+    <div class="form-group">
+        {!! Form::label('name', 'Name') !!}
+        {!! Form::text('name', null, ['class' => 'form-control']) !!}
+    </div>
+
+    <div class="form-group">
+        {!! Form::label('email', 'Email') !!}
+        {!! Form::email('email', null, ['class' => 'form-control']) !!}
+    </div>
+
+    <div class="form-group">
+        {!! Form::label('role_id', 'Role') !!}
+        {!! Form::select('role_id', ['' => 'Choose role'] + $roles, null,['class' => 'form-control']) !!}
+    </div>
+
+    <div class="form-group">
+        {!! Form::label('is_active', 'Status') !!}
+        {!! Form::select('is_active', ['Not Active', 'Active'], 0, ['class' => 'form-control']) !!}
+    </div>
+
+    <div class="form-group">
+        {!! Form::label('file', 'Image') !!}
+        {!! Form::file('file', ['class' => 'form-control']) !!}
+    </div>
+
+    <div class="form-group">
+        {!! Form::label('password', 'Password') !!}
+        {!! Form::password('password', ['class' => 'form-control']) !!}
+    </div>
+
+    <div class="form-group">
+        {!! Form::submit('create', ['class' => 'btn btn-primary']) !!}
+    </div>
+
+    {!! Form::close() !!}
+
+    @include('includes.form_error')
+
+@endsection
